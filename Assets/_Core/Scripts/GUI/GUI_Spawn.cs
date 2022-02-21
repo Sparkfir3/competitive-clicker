5e196f05f0963413f028ba7fa5ed6ac42b223b83
Richard <richard@thedevhouseagency.com>
2022-02-21 14:31:13 -0600

    Cursor
    Cursor for selecting generators and spawn locations

---
+++ Assets/_Core/Scripts/GUI/GUI_Spawn.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GUI_Spawn : MonoBehaviour
+{
+    // The spawn objects should be hidden when you're not
+    // spawning a new unit
+}
